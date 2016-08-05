953c49120c18f58457ac8956f9078619195f1939
namirda <namirda@hotmail.com>
2016-08-05 15:32:53 +1200

    Add files via upload

--- _P210_MQTTImport.ino
+++ _P210_MQTTImport.ino
@@ -1,9 +1,10 @@
 //#######################################################################################################
 //#################################### Plugin 210: MQTT Import ##########################################
-//#################################### Version 0.3 12-May-2016 ##########################################
+//#################################### Version 0.4 5-Aug-2016 ###########################################
+//##         This Version Requires ESPEasy 114+, Core 2.3 and PubSub 2.6                          #######
 //#######################################################################################################
 
-// This simple task reads data from the MQTT Import  input stream and saves the value
+// This task reads data from the MQTT Import input stream and saves the value
 
 #define PLUGIN_210
 #define PLUGIN_ID_210         210
@@ -15,22 +16,17 @@
 #define PLUGIN_VALUENAME4_210 "Value4"
 
 #define PLUGIN_IMPORT 210		// This is a 'private' function used only by this import module
+ 
+// Declare a Wifi client for this plugin only
 
-// The line below defines the dummy function PLUGIN_COMMAND which is only for Namirda use
-
-#ifndef PLUGIN_COMMAND
-#define PLUGIN_COMMAND 999
-#endif    
-
-// Define MQTT client for this plugin only
-
-PubSubClient MQTTclient_210("");		// Create a new pubsub instance
+WiFiClient espclient_210;
+PubSubClient MQTTclient_210(espclient_210);		// Create a new pubsub instance
 
 boolean Plugin_210(byte function, struct EventStruct *event, String& string)
 {
   boolean success = false;
+
   char deviceTemplate[4][41];		// variable for saving the subscription topics
-  
   //
   // Generate the MQTT import client name from the system name and a suffix
   //
@@ -119,7 +115,7 @@ boolean Plugin_210(byte function, struct EventStruct *event, String& string)
 		if (MQTTConnect_210(ClientName))
 		{
 //		Subscribe to ALL the topics from ALL instance of this import module
-			MQTTsubscribe();
+			MQTTSubscribe_210();
 			success = true;
 		}
 		else 
@@ -141,7 +137,7 @@ boolean Plugin_210(byte function, struct EventStruct *event, String& string)
 
           if (!MQTTclient_210.connected()){
           
-            String log = F("CON  : MQTT 210 Connection lost");
+            String log = F("IMPT : MQTT 210 Connection lost");
             addLog(LOG_LEVEL_ERROR, log);
               
             MQTTclient_210.disconnect();
@@ -152,7 +148,7 @@ boolean Plugin_210(byte function, struct EventStruct *event, String& string)
               break;
             }
 
-            MQTTsubscribe();
+            MQTTSubscribe_210();
           }     
         
           success=true;
@@ -162,47 +158,10 @@ boolean Plugin_210(byte function, struct EventStruct *event, String& string)
     case PLUGIN_READ:
       {
         // This routine does not output any data and so we do not need to respond to regular read requests
+
         success = false;
         break;
       }
-
-    case PLUGIN_COMMAND:
-      {
-        // This option is called when user has made a request to this task
-        
-//      Get the payload
-
-        String Payload=event->String2;
-        float floatPayload=string2float(Payload);
-
-//      Abort if not a valid number
-
-        if (floatPayload == -999)
-        {
-          String log=F("ERR  : Illegal value for Payload ");
-          log+=Payload;
-          log += " - must be a valid floating point number";
-          addLog(LOG_LEVEL_INFO,log);
-          break;
-        }
-        
-//      Get the valuenameindex for this command
-        
-        byte ValueNameIndex=getValueNameIndex(event->TaskIndex,string);
-        
-        if (ValueNameIndex == 255){
-          addLog(LOG_LEVEL_INFO,"ERR  : Internal Error");
-          break;
-        }
-        
-        UserVar[event->BaseVarIndex+ValueNameIndex]=floatPayload;  // Save the new value
-
-        logUpdates(210,event->TaskIndex,ValueNameIndex,floatPayload);
-
-        success=true;
-        break;
-    }
-  
 
     case PLUGIN_IMPORT:
       {
@@ -216,7 +175,7 @@ boolean Plugin_210(byte function, struct EventStruct *event, String& string)
         LoadTaskSettings(event->TaskIndex);
                      
         if (floatPayload == -999){
-          String log=F("ERR  : Bad Import MQTT Command ");
+          String log=F("IMPT : Bad Import MQTT Command ");
           log+=event->String1;
           addLog(LOG_LEVEL_ERROR,log);
           log="ERR  : Illegal Payload ";
@@ -242,23 +201,34 @@ boolean Plugin_210(byte function, struct EventStruct *event, String& string)
 
 			// Now check if the incoming topic matches one of our subscriptions
 
-            if (MQTTCheckSubscription(Topic,subscriptionTopic))
+            if (MQTTCheckSubscription_210(Topic,subscriptionTopic))
 			{	
               UserVar[event->BaseVarIndex+x]=floatPayload;							// Save the new value
-              logUpdates(210,event->TaskIndex,x,UserVar[event->BaseVarIndex+x]);    // Log the event
+
+              // Log the event
+
+			  String log =F("IMPT : [");
+			  log += ExtraTaskSettings.TaskDeviceName;
+			  log += F("#");
+			  log += ExtraTaskSettings.TaskDeviceValueNames[x];
+			  log += F("] : ");
+			  log += floatPayload;
+			  addLog(LOG_LEVEL_INFO, log);
 
 			  // Generate event for rules processing - proposed by TridentTD
 			  
 			  if (Settings.UseRules)
 			  {
-				  String event = F("");
-				  event += ExtraTaskSettings.TaskDeviceName;
-				  event += "#";
-				  event += ExtraTaskSettings.TaskDeviceValueNames[x];
-				  event += "=";
-				  event += floatPayload;
-				  rulesProcessing(event);
+				  String RuleEvent = F("");
+				  RuleEvent += ExtraTaskSettings.TaskDeviceName;
+				  RuleEvent += F("#");
+				  RuleEvent += ExtraTaskSettings.TaskDeviceValueNames[x];
+				  RuleEvent += F("=");
+				  RuleEvent += floatPayload;
+				  rulesProcessing(RuleEvent);
 			  }
+
+			  success = true;
             }
         }
 
@@ -269,128 +239,150 @@ boolean Plugin_210(byte function, struct EventStruct *event, String& string)
 
   return success;
 }
+boolean MQTTSubscribe_210()
+{
 
-//
-//	Subscribe to the topics requested by ALL calls to this plugin
-//
-void MQTTsubscribe(){
+	// Subscribe to the topics requested by ALL calls to this plugin.
+	// We do this because if the connection to the broker is lost, we want to resubscribe for all instances.
 
-// Subscribe to the topics requested by ALL calls to this plugin.
-// We do this because if the connection to the broker is lost, we want to resubscribe for all instances.
+	char deviceTemplate[4][41];
 
-  char deviceTemplate[4][41];
+	//	Loop over all tasks looking for a 210 instance
 
-//	Loop over all tasks looking for a 210 instance
+	for (byte y = 0; y < TASKS_MAX; y++)
+	{
+		if (Settings.TaskDeviceNumber[y] == 210)
+		{
+			LoadCustomTaskSettings(y, (byte*)&deviceTemplate, sizeof(deviceTemplate));
 
-  for (byte y = 0; y < TASKS_MAX; y++){
+			// Now loop over all import variables and subscribe to those that are not blank
 
-    if (Settings.TaskDeviceNumber[y] == 210) {
-      LoadCustomTaskSettings(y, (byte*)&deviceTemplate, sizeof(deviceTemplate));
+			for (byte x = 0; x < 4; x++)
+			{
+				String subscribeTo = deviceTemplate[x];
 
-	  // Now loop over all import variables and subscribe to those that are not blank
+				if (subscribeTo.length() > 0)
+				{
+					if (MQTTclient_210.subscribe(subscribeTo.c_str()))
+					{
+						String log = F("IMPT : [");
+						LoadTaskSettings(y);
+						log += ExtraTaskSettings.TaskDeviceName;
+						log += F("#");
+						log += ExtraTaskSettings.TaskDeviceValueNames[x];
+						log += F("] subscribed to ");
+						log += subscribeTo;
+						addLog(LOG_LEVEL_INFO, log);
+					}
+					else
+					{
+						String log = F("IMPT : Error subscribing to ");
+						log += subscribeTo;
+						addLog(LOG_LEVEL_ERROR, log);
+						return false;
+					}
 
-      for (byte x = 0; x < 4; x++) {
-        String subscribeTo = deviceTemplate[x];
-        if (subscribeTo.length() > 0 ){
-              
-           MQTTclient_210.subscribe(subscribeTo);
-
-           String log=F("SUB  : [");
-           LoadTaskSettings(y);
-           log+=ExtraTaskSettings.TaskDeviceName;
-           log+="#";
-           log+=ExtraTaskSettings.TaskDeviceValueNames[x];
-           log+="] subscribed to ";
-           log+=subscribeTo;
-           addLog(LOG_LEVEL_INFO,log);
-        }
-      }
-    }
-  }
+				}
+			}
+		}
+	}
+	return true;
 }
 //
 // handle MQTT messages
 //
-void callback_210(const MQTT::Publish& pub) {
+void mqttcallback_210(char* c_topic, byte* b_payload, unsigned int length)
+{
+	// Here we have incomng MQTT messages from the mqtt import module
 
-// Here we have incomng MQTT messages from the mqtt import module
+	String topic = c_topic;
 
-  String topic = pub.topic();
-  String payload = pub.payload_string();
-  
-  byte DeviceIndex=getDeviceIndex(210);   // This is the device index of 210 modules -there should be one!
-  
-  // We generate a temp event structure to pass to the plugins
-  
-  struct EventStruct TempEvent;
+	char cpayload[256];
+	strncpy(cpayload, (char*)b_payload, length);
+	cpayload[length] = 0;
+	String payload = cpayload;		// convert byte to char string
+	payload.trim();
 
-  TempEvent.String1=topic;                            // This is the topic of the message
-  TempEvent.String2=payload;                          // This is the payload
+	byte DeviceIndex = getDeviceIndex(210);   // This is the device index of 210 modules -there should be one!
 
-//  Here we loop over all tasks and call each 210 plugin with function PLUGIN_IMPORT
+	// We generate a temp event structure to pass to the plugins
 
-  for (byte y = 0; y < TASKS_MAX; y++)
-  {
-      if (Settings.TaskDeviceNumber[y] == 210){                 // if we have found a 210 device, then give it something to think about!
-            TempEvent.TaskIndex=y;
-            TempEvent.BaseVarIndex=y * VARS_PER_TASK;           // This is the index in Uservar where values for this task are stored
-            Plugin_ptr[DeviceIndex](PLUGIN_IMPORT, &TempEvent, payload);
-      }      
-  }
+	struct EventStruct TempEvent;
+
+	TempEvent.String1 = topic;                            // This is the topic of the message
+	TempEvent.String2 = payload;                          // This is the payload
+
+	//  Here we loop over all tasks and call each 210 plugin with function PLUGIN_IMPORT
+
+	for (byte y = 0; y < TASKS_MAX; y++)
+	{
+		if (Settings.TaskDeviceNumber[y] == 210)                // if we have found a 210 device, then give it something to think about!
+		{
+			TempEvent.TaskIndex = y;
+			TempEvent.BaseVarIndex = y * VARS_PER_TASK;           // This is the index in Uservar where values for this task are stored
+			Plugin_ptr[DeviceIndex](PLUGIN_IMPORT, &TempEvent, payload);
+		}
+	}
 }
 
 //
-// Make a new client connection to the mqtt broker
-//
+// Make a new client connection to the mqtt broker.
+// For some reason this seems to failduring the call in INIT- however it succeeds later during recovery
+// It would be nice to understand this....
+
 boolean MQTTConnect_210(String clientid)
 
 {
-  boolean result=false;
+	boolean result = false;
 
-// Do nothing if already connected
+	// Do nothing if already connected
 
-  if (MQTTclient_210.connected()){
-    result=true;
-    return result;
-  }
-  
-  IPAddress MQTTBrokerIP(Settings.Controller_IP);
-  MQTTclient_210.set_server(MQTTBrokerIP, Settings.ControllerPort);
-  MQTTclient_210.set_callback(callback_210);
+	if (MQTTclient_210.connected())return true;
 
-//  Try three times for a connection
+	IPAddress MQTTBrokerIP(Settings.Controller_IP);
 
-  for (byte x = 1; x < 3; x++)
-  {
-    String log = "";
+	// define stuff for the client - this could also be done in the intial declaration of MQTTclient_210
 
-    if ((SecuritySettings.ControllerUser[0] != 0) && (SecuritySettings.ControllerPassword[0] != 0))
-      result = (MQTTclient_210.connect(MQTT::Connect(clientid).set_auth(SecuritySettings.ControllerUser, SecuritySettings.ControllerPassword)));
-    else
-      result = (MQTTclient_210.connect(clientid));
+	MQTTclient_210.setServer(MQTTBrokerIP, Settings.ControllerPort);
+	MQTTclient_210.setCallback(mqttcallback_210);
 
-    if (result)
-    {
-      log = F("CON  : Import MQTT Connected to broker with Client ID=");
-	  log += clientid;
-      addLog(LOG_LEVEL_INFO, log);
-      
-      break; // end loop if succesfull
-    }
-    else
-    {
-      log = F("CON  : Import MQTT Failed to connect to broker");
-      addLog(LOG_LEVEL_ERROR, log);
-    }
+	//  Try three times for a connection
 
-    delay(500);
-  }
-  return result;
+	for (byte x = 1; x < 4; x++)
+	{
+		String log = "";
+
+		if ((SecuritySettings.ControllerUser[0] != 0) && (SecuritySettings.ControllerPassword[0] != 0))
+			result = MQTTclient_210.connect(clientid.c_str(), SecuritySettings.ControllerUser, SecuritySettings.ControllerPassword);
+		else
+			result = MQTTclient_210.connect(clientid.c_str());
+
+
+		if (result)
+		{
+			log = F("IMPT : Connected to MQTT broker with Client ID=");
+			log += clientid;
+			addLog(LOG_LEVEL_INFO, log);
+
+			break; // end loop if succesfull
+		}
+		else
+		{
+			log = F("IMPT : Failed to connect to MQTT broker - attempt ");
+			log += x;
+			addLog(LOG_LEVEL_ERROR, log);
+		}
+
+		delay(500);
+	}
+
+	return MQTTclient_210.connected();
 }
+
 //
 // Check to see if Topic matches the MQTT subscription
 //
-boolean MQTTCheckSubscription(String Topic, String Subscription) {
+boolean MQTTCheckSubscription_210(String Topic, String Subscription) {
 
 	String tmpTopic = Topic;
 	String tmpSub = Subscription;
@@ -406,10 +398,10 @@ boolean MQTTCheckSubscription(String Topic, String Subscription) {
 	// Add trailing / if required
 
 	int lenTopic = tmpTopic.length();
-	if (tmpTopic.substring(lenTopic - 1, lenTopic) != "/")tmpTopic += "/";
+	if (tmpTopic.substring(lenTopic - 1, lenTopic) != "/")tmpTopic += F("/");
 
 	int lenSub = tmpSub.length();
-	if (tmpSub.substring(lenSub - 1, lenSub) != "/")tmpSub += "/";
+	if (tmpSub.substring(lenSub - 1, lenSub) != "/")tmpSub += F("/");
 
 	// Now get first part
 
@@ -453,5 +445,42 @@ boolean MQTTCheckSubscription(String Topic, String Subscription) {
 	return false;
 }
 
+//	***************************************************************************
+// Convert String to float - returns -999 in case of error
+float string2float(String myString) {
+	int i, len;
+	float value;
+	len = myString.length();
+	char tmp[(len + 1)];       // one extra for the zero termination
+	byte start = 0;
 
+	//  Look for decimal point - they can be anywhere but no more than one of them!
 
+	int dotIndex = myString.indexOf(".");
+	//Serial.println(dotIndex);
+
+	if (dotIndex != -1)
+	{
+		int dotIndex2 = (myString.substring(dotIndex + 1)).indexOf(".");
+		//Serial.println(dotIndex2);
+		if (dotIndex2 != -1)return -999.00;    // Give error if there is more than one dot
+	}
+
+	if (myString.substring(0, 1) == "-") {
+		start = 1;   //allow a minus in front of string
+		tmp[i] = '-';
+	}
+
+	for (i = start; i<len; i++)
+	{
+		tmp[i] = myString.charAt(i);
+		if (!isdigit(tmp[i]))
+		{
+			if (tmp[i] != '.')return -999;
+		}
+	}
+
+	tmp[i] = 0;
+	value = atof(tmp);
+	return value;
+}
