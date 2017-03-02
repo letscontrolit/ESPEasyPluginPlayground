879dcb1418274f86a35a48cf4f95d8003886ae05
sebastien <sebasto@gmail.com>
2017-03-02 18:21:07 +0100

    Improve MHZ19 read stability

--- _P149_MHZ19.ino
+++ _P149_MHZ19.ino
@@ -13,6 +13,7 @@
 #define PLUGIN_ID_149         149
 #define PLUGIN_NAME_149       "NDIR CO2 Sensor MH-Z19"
 #define PLUGIN_VALUENAME1_149 "PPM"
+#define PLUGIN_READ_TIMEOUT   3000
 
 boolean Plugin_149_init = false;
 
@@ -25,7 +26,7 @@ byte mhzResp[9];    // 9 bytes bytes response
 
 boolean Plugin_149(byte function, struct EventStruct *event, String& string)
 {
-  boolean success = false;
+  bool success = false;
 
   switch (function)
   {
@@ -60,8 +61,12 @@ boolean Plugin_149(byte function, struct EventStruct *event, String& string)
 
     case PLUGIN_INIT:
       {
-        Plugin_149_init = true;
         Plugin_149_S8 = new SoftwareSerial(Settings.TaskDevicePin1[event->TaskIndex], Settings.TaskDevicePin2[event->TaskIndex]); 
+        Plugin_149_S8->begin(9600);
+        String log = F("MHZ19:  Init OK ");
+        addLog(LOG_LEVEL_INFO, log);
+
+        Plugin_149_init = true;
         success = true;
         break;
       }
@@ -71,30 +76,54 @@ boolean Plugin_149(byte function, struct EventStruct *event, String& string)
 
         if (Plugin_149_init)
         {
-          Plugin_149_S8->write(mhzCmd, 9);
+          //send read PPM command
+          int nbBytesSent = Plugin_149_S8->write(mhzCmd, 9);
+          if (nbBytesSent != 9) {
+            String log = F("MHZ19: Error, nb bytes sent != 9 : ");
+              log += nbBytesSent;
+              addLog(LOG_LEVEL_INFO, log);
+          }
+
+          // get response
           memset(mhzResp, 0, 9);
-          Plugin_149_S8->readBytes(mhzResp, 9);
-          int i;
+
+          long start = millis();
+          int counter = 0;
+          while (((millis() - start) < PLUGIN_READ_TIMEOUT) && (counter < 9)) {
+            if (Plugin_149_S8->available() > 0) {
+              mhzResp[counter++] = Plugin_149_S8->read();
+            } else {
+              delay(10);
+            }
+          }
+          if (counter < 9){
+              String log = F("MHZ19: Error, timeout while trying to read");
+              addLog(LOG_LEVEL_INFO, log);
+          }
+       
           unsigned int ppm = 0;
+          int i;
           byte crc = 0;
           for (i = 1; i < 8; i++) crc+=mhzResp[i];
               crc = 255 - crc;
               crc++;
               
           if ( !(mhzResp[0] == 0xFF && mhzResp[1] == 0x86 && mhzResp[8] == crc) ) {
-             String log = F("MHZ19: CRC error: ");
+             String log = F("MHZ19: Read error : CRC = ");
              log += String(crc); log += " / "; log += String(mhzResp[8]);
+             log += " bytes read  => ";for (i = 0; i < 9; i++) {log += mhzResp[i];log += "/" ;}
              addLog(LOG_LEVEL_ERROR, log);
+
              success = false;
              break;
              
           } else {
-            
+              //calculate CO2 PPM
               unsigned int mhzRespHigh = (unsigned int) mhzResp[2];
               unsigned int mhzRespLow = (unsigned int) mhzResp[3];
               ppm = (256*mhzRespHigh) + mhzRespLow;
           }
-          
+
           UserVar[event->BaseVarIndex] = (float)ppm;
           String log = F("MHZ19: PPM value: ");
           log += ppm;
