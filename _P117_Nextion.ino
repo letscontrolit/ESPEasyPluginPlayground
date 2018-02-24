17d8a042335e9d6463ab1337d46fa3b3b189db85
berbergh <berbergh@gmail.com>
2018-02-24 18:44:28 +0100

    Removed some bugs

--- _P117_Nextion.ino
+++ _P117_Nextion.ino
@@ -2,7 +2,8 @@
 #include <SoftwareSerial.h>
 
 //#######################################################################################################
-//#################################### Plugin 117: Nextion <info@sensorio.cz>  ###########################
+//################################### Plugin 117: Nextion <info@sensorio.cz>  ###########################
+//################################### Created on the work of  majklovec       ###########################
 //#######################################################################################################
 
 #define PLUGIN_117
@@ -12,12 +13,12 @@
 unsigned long Plugin_117_code = 0;
 int8_t Plugin_117_RXpin = -1;
 int8_t Plugin_117_TXpin = -1;
+#define Nlines 12        // The number of different lines which can be displayed - each line is 32 chars max
 
 SoftwareSerial *nextion;
 
 boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
   boolean success = false;
-  static byte displayTimer = 0;
 
   switch (function) {
 
@@ -25,12 +26,12 @@ boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
       Device[++deviceCount].Number = PLUGIN_ID_117;
 
       Device[deviceCount].Type = DEVICE_TYPE_DUAL;
-      Device[deviceCount].VType = SENSOR_TYPE_QUAD;
+      Device[deviceCount].VType = SENSOR_TYPE_DUAL;
       Device[deviceCount].Ports = 0;
       Device[deviceCount].PullUpOption = true;
       Device[deviceCount].InverseLogicOption = false;
       Device[deviceCount].FormulaOption = false;
-      Device[deviceCount].ValueCount = 4;
+      Device[deviceCount].ValueCount = 2;
       Device[deviceCount].SendDataOption = true;
       Device[deviceCount].TimerOption = true;
       Device[deviceCount].GlobalSyncOption = true;
@@ -53,6 +54,11 @@ boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
 
       uint16_t i;
       uint8_t c;
+      String Vidx;
+      String Nvalue;
+      String Svalue;
+      String Nswitch;
+      
       while (nextion->available() > 0) {
         delay(10);
         c = nextion->read();
@@ -83,36 +89,66 @@ boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
           }
         } else {
 
-          if (c == 's') {
+          if (c == '|') {
             i = 1;
             __buffer[0] = c;
             c=0;
             while (nextion->available() > 0) {
-              __buffer[i++] = nextion->read();
-              Serial.write((char)__buffer[i]);
+              __buffer[i] = nextion->read();
+              if (__buffer[i]==0x0d) break;
+              i++;
             }
-            Serial.println();
             __buffer[i] = 0x00;
 
-            UserVar[event->BaseVarIndex] = __buffer[2];
-            UserVar[event->BaseVarIndex+1] = __buffer[3];
-            UserVar[event->BaseVarIndex+2] = __buffer[4];
-            UserVar[event->BaseVarIndex+3] = __buffer[5];
+            String tmpString = __buffer;
+
+            int argIndex = tmpString.indexOf(",i");
+            int argEnd = tmpString.indexOf(',', argIndex + 1);
+            if (argIndex)
+            Vidx = tmpString.substring(argIndex + 2,argEnd);
+
+            switch (__buffer[1]){
+            
+              case 'u':
+
+                argIndex = argEnd;
+                argEnd = tmpString.indexOf(',',argIndex + 1);
+                if (argIndex)
+                  Nvalue = tmpString.substring(argIndex + 2,argEnd);
+
+                argIndex = argEnd;
+                argEnd = tmpString.indexOf(0x0a);
+                if (argIndex)
+                  Svalue = tmpString.substring(argIndex + 2,argEnd);
+                
+                break;
+
+              case 's':
+      
+                argIndex = argEnd;
+                argEnd = tmpString.indexOf(0x0a);
+                if (argIndex)
+                  Nvalue = tmpString.substring(argIndex + 2,argEnd);
+                if (Nvalue=="On")
+                  Svalue='1';
+                if (Nvalue=="Off")
+                  Svalue='0';
+
+                break;
+      
+            }
+
+            UserVar[event->BaseVarIndex] = Vidx.toFloat();
+            UserVar[event->BaseVarIndex+1] = Svalue.toFloat();
 
             String log = F("Nextion : send command: ");
             log += __buffer;
             log += UserVar[event->BaseVarIndex];
             addLog(LOG_LEVEL_INFO, log);
+            sendData(event);
             
             ExecuteCommand(VALUE_SOURCE_SYSTEM, __buffer);
           }
-        }
-      }
-
-      if (Settings.TaskDevicePin3[event->TaskIndex] != -1) {
-        if (!digitalRead(Settings.TaskDevicePin3[event->TaskIndex])) {
-          Plugin_117_displayOn();
-          displayTimer = Settings.TaskDevicePluginConfig[event->TaskIndex][2];
         }
       }
 
@@ -121,40 +157,29 @@ boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
     }
 
     case PLUGIN_ONCE_A_SECOND: {
-      if ( displayTimer > 0) {
-        displayTimer--;
-        if (displayTimer == 0)
-          Plugin_117_displayOff();
-        }
+        success = true;
         break;
       }
 
 
     case PLUGIN_WEBFORM_SAVE: {
 
-      String plugin1 = WebServer.arg("plugin_117_timer");
-      Settings.TaskDevicePluginConfig[event->TaskIndex][0] = plugin1.toInt();
-      String plugin2 = WebServer.arg("plugin_117_rxwait");
-      Settings.TaskDevicePluginConfig[event->TaskIndex][1] = plugin2.toInt();
-      String plugin3 = WebServer.arg("plugin_117_events");
-      Settings.TaskDevicePluginConfig[event->TaskIndex][2] = plugin3.toInt();
-      String plugin4 = WebServer.arg("plugin_117_sensortype");
-      Settings.TaskDevicePluginConfig[event->TaskIndex][3] = plugin4.toInt();
+        String argName;
+        
+        char deviceTemplate[Nlines][64];
+        for (byte varNr = 0; varNr < Nlines; varNr++)
+        {
+          String arg = F("Plugin_117_template");
+          arg += varNr + 1;
+          String tmpString = WebServer.arg(arg);
+          strncpy(deviceTemplate[varNr], tmpString.c_str(), sizeof(deviceTemplate[varNr])-1);
+          deviceTemplate[varNr][63]=0;
+          
+//          strncpy(deviceTemplate[varNr], WebServer.arg(argName).c_str(), sizeof(deviceTemplate[varNr]));
+        }
 
-      char deviceTemplate[8][64];
-      for (byte varNr = 0; varNr < 8; varNr++) {
-        char argc[25];
-        String arg = F("Plugin_117_template");
-        arg += varNr + 1;
-        arg.toCharArray(argc, 25);
-        String tmpString = WebServer.arg(argc);
-        strncpy(deviceTemplate[varNr], tmpString.c_str(), sizeof(deviceTemplate[varNr]));
-      }
+        SaveCustomTaskSettings(event->TaskIndex, (byte*)&deviceTemplate, sizeof(deviceTemplate));
 
-//      Settings.TaskDeviceID[event->TaskIndex] = 1; // temp fix, needs a dummy value
-
-      SaveCustomTaskSettings(event->TaskIndex, (byte*)&deviceTemplate, sizeof(deviceTemplate));
-    
       success = true;
       break;
     }
@@ -162,26 +187,14 @@ boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
 
     case PLUGIN_WEBFORM_LOAD: {
 
-      char tmpString[128];
-
-      char deviceTemplate[8][64];
+      char deviceTemplate[Nlines][64];
       LoadCustomTaskSettings(event->TaskIndex, (byte*)&deviceTemplate, sizeof(deviceTemplate));
-      for (byte varNr = 0; varNr < 8; varNr++)
-        {
-          string += F("<TR><TD>Line ");
-          string += varNr + 1;
-          string += F(":<TD><input type='text'li size='50' maxlength='50' name='Plugin_117_template");
-          string += varNr + 1;
-          string += F("' value='");
-          string += deviceTemplate[varNr];
-          string += F("'>");
-        }
+      
+      for (byte varNr = 0; varNr < Nlines; varNr++)
+      {
+        addFormTextBox(string, String(F("Line ")) + (varNr + 1), String(F("Plugin_117_template")) + (varNr + 1), deviceTemplate[varNr], 64);
+      }
 
-      string += F("<TR><TD>Display button:<TD>");
-      addPinSelect(false, string, "taskdevicepin3", Settings.TaskDevicePin3[event->TaskIndex]);
-
-      sprintf_P(tmpString, PSTR("<TR><TD>Display Timeout:<TD><input type='text' name='plugin_117_timer' value='%u'>"), Settings.TaskDevicePluginConfig[event->TaskIndex][2]);
-      string += tmpString;
 
       success = true;
       break;
@@ -190,63 +203,67 @@ boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
     case PLUGIN_INIT: {
 
       LoadTaskSettings(event->TaskIndex);
-//      if ((ExtraTaskSettings.TaskDevicePluginConfigLong[0] != 0) && (ExtraTaskSettings.TaskDevicePluginConfigLong[1] != 0)) {
-//      if (Settings.TaskDevicePin1[event->TaskIndex] != -1) {
-//        pinMode(Settings.TaskDevicePin1[event->TaskIndex], OUTPUT);
-//       digitalWrite(Settings.TaskDevicePin1[event->TaskIndex], LOW);
-//        delay(500);
-//        digitalWrite(Settings.TaskDevicePin1[event->TaskIndex], HIGH);
-//        pinMode(Settings.TaskDevicePin1[event->TaskIndex], INPUT_PULLUP);
-//      }
-
-      displayTimer = Settings.TaskDevicePluginConfig[event->TaskIndex][2];
-      if (Settings.TaskDevicePin3[event->TaskIndex] != -1)
-          pinMode(Settings.TaskDevicePin3[event->TaskIndex], INPUT_PULLUP);
 
       if (Settings.TaskDevicePin1[event->TaskIndex] != -1)
         {
           Plugin_117_RXpin = Settings.TaskDevicePin1[event->TaskIndex];
-//          pinMode(Plugin_117_RXpin, INPUT_PULLUP);
-//          attachInterrupt(Plugin_117_RXpin, RF_ISR, CHANGE);
-          success = true;
         }
 
       if (Settings.TaskDevicePin2[event->TaskIndex] != -1)
         {
           Plugin_117_TXpin = Settings.TaskDevicePin2[event->TaskIndex];
-//          pinMode(Plugin_117_TXpin, OUTPUT);
         }
       
       if (!nextion) {
-//        nextion = new SoftwareSerial(13,14);
         nextion = new SoftwareSerial(Plugin_117_RXpin , Plugin_117_TXpin);
       }
 
-      Serial.print("pin: ");
-      Serial.println(Settings.TaskDevicePin1[event->TaskIndex]);
-      Serial.println(Settings.TaskDevicePin2[event->TaskIndex]);
-      Serial.println(Plugin_117_RXpin);
-      Serial.println(Plugin_117_TXpin);
-
       nextion->begin(9600);
-
+ 
       success = true;
       break;
     }
 
+
     case PLUGIN_READ: {
-        char deviceTemplate[8][64];
+        char deviceTemplate[Nlines][64];
         LoadCustomTaskSettings(event->TaskIndex, (byte*)&deviceTemplate, sizeof(deviceTemplate));
-
-        for (byte x = 0; x < 8; x++) {
+        String newString;
+        
+        for (byte x = 0; x < Nlines; x++) {
           String tmpString = deviceTemplate[x];
-          if (tmpString.length()) {
-            String newString = parseTemplate(tmpString, 0);
-            Serial.println(newString);
+          if (tmpString.length())
+          {
+            int rssiIndex = tmpString.indexOf("rssi");
+            if(rssiIndex >= 0)
+            {
+              int barVal;
+              newString = tmpString.substring(0, rssiIndex);
+              int nbars = WiFi.RSSI();
+              if (nbars < -100)
+                 barVal=0;
+              else if (nbars >= -100 and nbars < -90)
+                 barVal=20;
+              else if (nbars >= -90 and nbars < -80)
+                 barVal=40;
+              else if (nbars >= -80 and nbars < -70)
+                 barVal=60;
+              else if (nbars >= -70 and nbars < -60)
+                 barVal=80;
+              else if (nbars >= -60)
+                 barVal=100;
 
-            sendCommand(newString.c_str());
+              newString += String(barVal,DEC);
             }
+            else
+            {
+              newString = parseTemplate(tmpString, 0);
+            }
+            
+            sendCommand(newString.c_str());
+          }
         }
+
         success = false;
         break;
       }
@@ -272,15 +289,6 @@ boolean Plugin_117(byte function, struct EventStruct *event, String &string) {
   return success;
 }
 
-
-
-
-void Plugin_117_displayOn() {
-}
-
-void Plugin_117_displayOff() {
-  
-}
 
 void sendCommand(const char *cmd) {
   nextion->print(cmd);
