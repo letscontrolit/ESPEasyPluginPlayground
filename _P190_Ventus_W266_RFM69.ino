baf77ac526c9312cfd2550b947473fe2c62c6d07
chunter1 <16907942+chunter1@users.noreply.github.com>
2023-05-08 11:37:10 +0200

    Update _P190_Ventus_W266_RFM69.ino
    PluginStats set to false

--- _P190_Ventus_W266_RFM69.ino
+++ _P190_Ventus_W266_RFM69.ino
@@ -1189,7 +1189,7 @@ boolean Plugin_190(uint8_t function, struct EventStruct *event, String& string)
         Device[deviceCount].ValueCount = 3;
         Device[deviceCount].TimerOption = false;
         Device[deviceCount].TimerOptional = false;
-        Device[deviceCount].PluginStats = true;
+        Device[deviceCount].PluginStats = false;
         break;
       }
 
